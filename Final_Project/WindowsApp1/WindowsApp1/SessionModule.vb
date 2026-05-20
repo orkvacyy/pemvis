d9de2ebc81db6ca99725cfbb19f89e7ba0e3819d
Takejude <sifwahfsofwani@gmail.com>
2026-05-20 22:26:58 +0800

    hapus duplikat

--- Final_Project/WindowsApp1/WindowsApp1/SessionModule.vb
+++
@@ -1,9 +0,0 @@
-﻿Module SessionModule
-    Public CurrentUserId As Integer = 0
-    Public CurrentUsername As String = ""
-
-    Public Sub Logout()
-        CurrentUserId = 0
-        CurrentUsername = ""
-    End Sub
-End Module
